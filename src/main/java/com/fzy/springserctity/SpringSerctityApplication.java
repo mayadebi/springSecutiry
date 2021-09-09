d094cb0716f5703f5a05925acc77d57a3503b028
mayadebi <503991583@qq.com>
2021-09-09 21:09:47 +0800

    新建springsecutiry项目

---
+++ src/main/java/com/fzy/springserctity/SpringSerctityApplication.java
@@ -0,0 +1,13 @@
+package com.fzy.springserctity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSerctityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSerctityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fzy/springserctity/SpringSerctityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fzy.springserctity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSerctityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
